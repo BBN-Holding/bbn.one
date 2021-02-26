b271e77ceafd53a9b7f361ebbb8798918b1ee75b
lucsoft <mail@lucsoft.de>
2021-02-27 00:39:37 +0100

    Added Opener

---
+++ pages/index/components/opener.ts
@@ -0,0 +1,22 @@
+import { span } from "@lucsoft/webgen";
+import '../../../assets/css/components/opener.css';
+import heroImage from '../../../assets/img/hero-img.png';
+
+export function renderOpener()
+{
+    const shell = document.createElement("div")
+    shell.classList.add('opener')
+    const leftSide = document.createElement('div')
+    const mainTitle = span("We represent your interests")
+    const subTitle = span("We support you in financing, distributing and marketing your music or video game all over the world.")
+    const button = document.createElement('a')
+    button.innerText = "Get started"
+    leftSide.append(mainTitle, subTitle, button)
+    const rightSide = document.createElement('div')
+    const image = document.createElement('img')
+    image.src = heroImage;
+    rightSide.append(image)
+    shell.append(leftSide, rightSide)
+
+    return shell;
+}
--- pages/index/index.ts
+++ pages/index/index.ts
@@ -1,7 +1,10 @@
 import '@lucsoft/webgen';
-import { WebGen } from "@lucsoft/webgen";
+import { SupportedThemes, WebGen } from "@lucsoft/webgen";
 import { renderNav } from "./components/nav";
 import '../../assets/css/main.css';
+import { renderOpener } from "./components/opener";
 const web = new WebGen({ autoLoadFonts: false });
 
-web.elements.body({ maxWidth: "69rem" }).custom(renderNav())
+web.elements.body({ maxWidth: "69rem" })
+    .custom(renderNav())
+    .custom(renderOpener())
