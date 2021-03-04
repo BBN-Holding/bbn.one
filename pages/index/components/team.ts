ee2ed407833f05096f4535c8b70b69d90e0968d3
lucsoft <mail@lucsoft.de>
2021-03-04 19:53:34 +0100

    Added Lucas Jrichardson as a team

--- pages/index/components/team.ts
+++ pages/index/components/team.ts
@@ -4,7 +4,9 @@ import team1 from '../../../assets/img/team/team-1.webp';
 import team2 from '../../../assets/img/team/team-2.webp';
 import team3 from '../../../assets/img/team/team-3.webp';
 import team4 from '../../../assets/img/team/team-4.webp';
-import { github, instagram, linkedIn, renderAction, twitter } from "./actions";
+import team5 from '../../../assets/img/team/team-5.png';
+
+import { github, instagram, link, linkedIn, renderAction, twitter } from "./actions";
 export function renderTeam(web: WebGen)
 {
     const data = document.createElement('article')
@@ -39,13 +41,20 @@ export function renderTeam(web: WebGen)
                 [ instagram, 'https://www.instagram.com/skidder6775/' ],
                 [ github, 'https://github.com/gregtcltk' ]
             ]),
-            renderPerson(team2, "Sarah Jhonson", "Product Manager"),
+            renderPerson(team5, "Lucas Jrichardson", "Chief Design Officer", [
+                [ linkedIn, "https://www.linkedin.com/in/lucsoft/" ],
+                [ twitter, "https://twitter.com/lucsoft_" ],
+                [ instagram, 'https://www.instagram.com/lucsoft_/' ],
+                [ github, 'https://github.com/lucsoft' ],
+                [ link, 'https://lucsoft.de' ]
+            ]),
             renderPerson(team3, "Maximilian Arzberger", "Chief Operating Officer", [
                 [ linkedIn, 'https://linkedin.com/in/maximilian-arzberger-5877a51b9' ],
                 [ twitter, 'https://twitter.com/Hax6775' ],
                 [ instagram, 'https://www.instagram.com/hax6775/' ],
                 [ github, 'https://github.com/Schlauer-Hax' ]
             ]),
+            renderPerson(team2, "Sarah Jhonson", "Product Manager"),
             renderPerson(team4, "Jana Holter", "Accountant")
         )
     return data;
