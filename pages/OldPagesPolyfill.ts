1fb1e02ddc2e04e4afe933b5c51ee4bf9fe4a536
lucsoft <mail@lucsoft.de>
2021-03-13 11:36:33 +0100

    Added some spacing in "old" pages

--- pages/OldPagesPolyfill.ts
+++ pages/OldPagesPolyfill.ts
@@ -1,1 +1,8 @@
-import { WebGen } from "@lucsoft/webgen"; new WebGen()
+import { WebGen } from "@lucsoft/webgen";
+
+const style = document.body.style;
+style.maxWidth = "80rem";
+style.margin = "5rem auto";
+style.padding = "0 1rem";
+style.display = "block";
+new WebGen();
