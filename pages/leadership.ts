fd4be03f0d15640e974c5bc6542eaa23b20f12f5
lucsoft <mail@lucsoft.de>
2022-02-18 15:55:47 +0100

    leadership: updated path

--- pages/leadership.ts
+++ pages/leadership.ts
@@ -1,9 +1,9 @@
-import { Vertical, View, WebGen } from "../../deps.ts";
-import { renderNav } from "../../components/nav.ts";
-import { renderTeam } from "../../components/team.ts";
-import { renderFooter } from "../../components/footer.ts";
+import { Vertical, View, WebGen } from "../deps.ts";
+import { renderNav } from "../components/nav.ts";
+import { renderTeam } from "../components/team.ts";
+import { renderFooter } from "../components/footer.ts";
 WebGen({ autoLoadFonts: false });
-import '../../assets/css/main.css';
+import '../assets/css/main.css';
 
 View(() => Vertical(
     renderNav(),
