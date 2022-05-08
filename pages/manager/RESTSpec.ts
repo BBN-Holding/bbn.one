eb168d146af4e817382ff09ed180585c954b9761
lucsoft <mail@lucsoft.de>
2022-05-08 16:41:15 +0200

    Added REST Spec for common backend and frontend

---
+++ pages/manager/RESTSpec.ts
@@ -0,0 +1,86 @@
+// deno-lint-ignore-file no-unused-vars
+import { delay } from "https://deno.land/std@0.138.0/async/mod.ts";
+type ArtistType = "PRIMARY";
+
+type Release = {
+    id: string,
+    type: "PUBLISHED" | "UNPUBLISHED" | "UNSUBMITTED",
+    title?: string,
+    upc?: string,
+    release?: string,
+    picture?: string,
+    artists?: [ name: string, img: string, type: ArtistType ][]
+};
+
+export const API = {
+    auth: {
+        refresh: {
+            post: async ({ refreshToken }: { refreshToken: string }) => {
+                await delay(1000);
+                return { accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlciI6MTAsIm5hbWUiOiJsdWNzb2Z0IiwibWFpbCI6Im1haWxAbHVjc29mdC5kZSIsInBpY3R1cmUiOiJodHRwczovL2x1Y3NvZnQuZGUvaW1nLzNEX2RhcmtfbHVjc29mdC5wbmciLCJpYXQiOjE1MTYyMzkwMjJ9.blMgwFi72mQZ4lxEAXeVfpK_pK6yJyZFyfAtn58xB-4" };
+            }
+        },
+        google: {
+            post: async ({ email, password }: { email: string, password: string }) => {
+                await delay(1000);
+                return { refreshToken: "refresh" };
+            }
+        },
+        email: {
+            post: async ({ email, password }: { email: string, password: string }) => {
+                await delay(1000);
+                return { refreshToken: "refresh" };
+            }
+        }
+    },
+    bbn: {
+        music: {
+            list: {
+                get: async () => {
+                    await delay(1000);
+                    return <Release[]>[
+                        {
+                            id: "id",
+                            type: "PUBLISHED",
+                            artists: [
+                                [ "joe", "biden", "PRIMARY" ]
+                            ],
+                        }
+                    ];
+                }
+            },
+            [ "{id}" ]: {
+                publish: {
+                    post: async (id: string) => {
+                        await delay(1000);
+                        return true;
+                    },
+                },
+                unpublish: {
+                    post: async (id: string) => {
+                        await delay(1000);
+                        return true;
+                    },
+                },
+                put: async (id: string, data: Release) => {
+                    await delay(1000);
+                    return true;
+                },
+                post: async (id: string, data: Release) => {
+                    await delay(1000);
+                    return true;
+                },
+                get: async (id: string) => {
+                    await delay(1000);
+                    return {
+                        id: "id",
+                        type: "PUBLISHED",
+                        artists: [
+                            [ "joe", "biden", "PRIMARY" ]
+                        ],
+                    };
+                }
+            }
+        }
+    }
+};
