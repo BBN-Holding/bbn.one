48a05d1e4d4d99a72b6e7d4bc128f922df34bf11
lucsoft <mail@lucsoft.de>
2022-05-24 17:25:10 +0200

    Removed reset dialog with an inline variant

--- pages/manager/signin.ts
+++ pages/manager/signin.ts
@@ -1,4 +1,4 @@
-import { Button, ButtonStyle, Color, Center, Custom, Dialog, Horizontal, img, Input, Page, PlainText, Spacer, Vertical, View, WebGen, ButtonComponent } from "../../deps.ts";
+import { Button, ButtonStyle, Color, Center, Custom, loadingWheel, Dialog, Horizontal, img, Input, Page, PlainText, Spacer, Vertical, View, WebGen, ButtonComponent, Box } from "../../deps.ts";
 import '../../assets/css/main.css';
 import '../../assets/css/signin.css';
 import '../../assets/css/components/subsidiaries.css';
@@ -13,39 +13,8 @@ Redirect();
 
 const para = new URLSearchParams(location.search)
 const { id, type } = { id: para.get("id"), type: para.get("type") };
-if (type == "forgot-password" && id) {
-    const dialog = Dialog<{ token: string, firstTime: boolean }>(({ update, state, use }) => {
-        if (!state.firstTime) {
-            API.auth.fromEmail.get(id).then(x => {
-                if (x.message)
-                    use(PlainText("Error: " + x.message).addClass("error-message"))
-                update({ token: x.JWT })
-            })
-            update({ firstTime: true })
-        }
-        return Vertical(
-            state.token ?
-                Page((formData) => [
-                    Input({
-                        placeholder: "New Password",
-                        ...syncFromData(formData, "password")
-                    }),
-                    Button("Update Password")
-                        .onPromiseClick(async () => {
-                            await API.user(state.token!).setMe.post({
-                                password: formData.get("password")?.toString()
-                            });
-                            dialog.remove()
-                        })
-                ]).getComponents()
-                : Center(PlainText("Loading..."))
-        );
-    })
-        .setTitle("Reset your Password")
-        .open()
-}
 
-View<{ error?: string, signup?: boolean }>(({ state, update }) => Vertical(
+View<{ error?: string, signup?: boolean, resetToken: string, loading: boolean }>(({ state, update }) => Vertical(
     DynaNavigation("Home"),
     Horizontal(
         Vertical(
@@ -54,48 +23,74 @@ View<{ error?: string, signup?: boolean }>(({ state, update }) => Vertical(
                 .addClass("line-header")
                 .setWidth("21rem")
                 .setFont(5.375, 800),
-            Button("Sign in with Google")
-                .setMargin("0 0 .8rem")
-                .setJustify("center")
-                .addPrefix(Custom(img(googleLog)).addClass("prefix-logo")),
-            Page((formData) => [
-                ...state.signup ? [ Input({ placeholder: "Name", type: "text", ...syncFromData(formData, "name") }) ] : [],
-                Input({ placeholder: "Email", type: "email", ...syncFromData(formData, "email") }),
-                Input({ placeholder: "Password", type: "password", ...syncFromData(formData, "password") }),
-                Button(state.signup ? "Register" : "Login")
-                    .onPromiseClick(handleSignUpInButton(formData, state, update))
-                    .setJustify("center"),
-                Horizontal(state.error ? PlainText(`Error: ${state.error}`).addClass("error-message") : null, Spacer()),
-                Horizontal(
-                    PlainText(state.signup ? "Known here?" : "New here?"),
-                    Button(state.signup ? "Sign in" : "Create a Account")
-                        .setStyle(ButtonStyle.Inline)
-                        .onClick(() => update({ signup: !state.signup }))
-                        .setColor(Color.Colored)
-                        .addClass("link"),
-                    Spacer()
-                )
-                    .setMargin("1rem 0 0"),
-                Horizontal(
-                    PlainText("Forgot your Password?"),
-                    Button("Reset it here")
-                        .setStyle(ButtonStyle.Inline)
-                        .setColor(Color.Colored)
-                        .onPromiseClick(async () => {
-                            await API.auth.forgotPassword.post({
-                                email: formData.get("email")?.toString() ?? "",
-                                redirect: location.href
-                            });
-                        })
-                        .addClass("link"),
-                    Spacer()
-                )
-            ]).disableAutoSpacerAtBottom().getComponents(),
+            (() => {
+                if (state.loading) return Box(Custom(loadingWheel() as Element as HTMLElement), PlainText("Loading...")).addClass("loading");
+                if (state.resetToken)
+                    return Page((formData) => [
+                        Input({ placeholder: "New Password", type: "password", ...syncFromData(formData, "password") }),
+                        Button("Reset your Password")
+                            .setJustify("center")
+                            .onPromiseClick(async () => {
+                                await API.user(state.resetToken!).setMe.post({
+                                    password: formData.get("password")?.toString()
+                                });
+                            }),
+                        PlainText(state.resetToken?.startsWith("!") ? "Error: Link is invalid" : "").addClass("error-message")
+                    ]).disableAutoSpacerAtBottom().getComponents();
+                return Page((formData) => [
+                    Button("Sign in with Google")
+                        .setMargin("0 0 .8rem")
+                        .setJustify("center")
+                        .addPrefix(Custom(img(googleLog)).addClass("prefix-logo")),
+                    ...state.signup ? [ Input({ placeholder: "Name", type: "text", ...syncFromData(formData, "name") }) ] : [],
+                    Input({ placeholder: "Email", type: "email", ...syncFromData(formData, "email") }),
+                    Input({ placeholder: "Password", type: "password", ...syncFromData(formData, "password") }),
+                    Button(state.signup ? "Register" : "Login")
+                        .onPromiseClick(handleSignUpInButton(formData, state, update))
+                        .setJustify("center"),
+                    Horizontal(state.error ? PlainText(`Error: ${state.error}`).addClass("error-message") : null, Spacer()),
+                    Horizontal(
+                        PlainText(state.signup ? "Known here?" : "New here?"),
+                        Button(state.signup ? "Sign in" : "Create a Account")
+                            .setStyle(ButtonStyle.Inline)
+                            .onClick(() => update({ signup: !state.signup }))
+                            .setColor(Color.Colored)
+                            .addClass("link"),
+                        Spacer()
+                    )
+                        .setMargin("1rem 0 0"),
+                    Horizontal(
+                        PlainText("Forgot your Password?"),
+                        Button("Reset it here")
+                            .setStyle(ButtonStyle.Inline)
+                            .setColor(Color.Colored)
+                            .onPromiseClick(async () => {
+                                await API.auth.forgotPassword.post({
+                                    email: formData.get("email")?.toString() ?? "",
+                                    redirect: location.href
+                                });
+                            })
+                            .addClass("link"),
+                        Spacer()
+                    )
+                ]).disableAutoSpacerAtBottom().getComponents();
+            })()
         ).setGap("11px"),
         Spacer()
     ).addClass("subsidiary-list"),
     Custom(img(heroImage)).addClass("background-image")
 ))
+    .change(({ update }) => {
+        if (type == "forgot-password" && id) {
+            update({ loading: true })
+            API.auth.fromEmail.get(id).then(x => {
+                if (x.message)
+                    update({ resetToken: "!" + x.message, loading: false })
+                else
+                    update({ resetToken: x.JWT, loading: false })
+            })
+        }
+    })
     .appendOn(document.body)
 
 function handleSignUpInButton(formData: FormData, state: Partial<{ error?: string | undefined; signup?: boolean | undefined; }>, update: (data: Partial<{ error?: string | undefined; signup?: boolean | undefined; }>) => void): (env: MouseEvent, e: ButtonComponent) => Promise<void> {
