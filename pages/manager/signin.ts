ab0b27dc6253cf9a1f5a6ee0f6fd3e780885d829
lucsoft <mail@lucsoft.de>
2022-07-02 13:13:14 +0200

    Made Required Inputs more clear

--- pages/manager/signin.ts
+++ pages/manager/signin.ts
@@ -1,4 +1,4 @@
-import { Button, ButtonStyle, Color, Custom, loadingWheel, Horizontal, img, Input, Page, PlainText, Spacer, Vertical, View, WebGen, ButtonComponent, Box } from "../../deps.ts";
+import { Button, ButtonStyle, Color, Custom, loadingWheel, Horizontal, img, Input, Page, PlainText, Spacer, Vertical, View, WebGen, ButtonComponent, Box, Component } from "../../deps.ts";
 import '../../assets/css/main.css';
 import '../../assets/css/signin.css';
 import heroImage from '../../assets/img/hero-img.png';
@@ -13,7 +13,7 @@ Redirect();
 const para = new URLSearchParams(location.search);
 const { id, type, state, code } = { id: para.get("id"), type: para.get("type"), state: para.get("state"), code: para.get("code") };
 
-View<{ error?: string, signup?: boolean, resetToken?: string, loading: boolean, password: string; }>(({ state, update }) => Vertical(
+View<{ mode: "login" | "register" | "reset-password"; error?: string, signup?: boolean, resetToken?: string, loading: boolean, password: string; }>(({ state, update }) => Vertical(
     DynaNavigation("Home"),
     Horizontal(
         Vertical(
@@ -43,36 +43,73 @@ View<{ error?: string, signup?: boolean, resetToken?: string, loading: boolean, 
                         .setJustify("center")
                         .asLinkButton(`${API.BASE_URL}auth/google-redirect?redirect=${location.href}&type=google-auth`)
                         .addPrefix(Custom(img(googleLog)).addClass("prefix-logo")),
-                    ...state.signup ? [ Input({ placeholder: "Name", type: "text", ...syncFromData(formData, "name") }) ] : [],
-                    Input({ placeholder: "Email", type: "email", ...syncFromData(formData, "email") }),
-                    Input({ placeholder: "Password", type: "password", ...syncFromData(formData, "password"), value: state.password }),
-                    Button(state.signup ? "Register" : "Login")
-                        .onPromiseClick(handleSignUpInButton(formData, state, update))
-                        .setJustify("center"),
                     Horizontal(state.error ? PlainText(`Error: ${state.error}`).addClass("error-message") : null, Spacer()),
-                    Horizontal(
-                        PlainText(state.signup ? "Known here?" : "New here?"),
-                        Button(state.signup ? "Sign in" : "Create a Account")
-                            .setStyle(ButtonStyle.Inline)
-                            .onClick(() => update({ signup: !state.signup }))
-                            .setColor(Color.Colored)
-                            .addClass("link"),
-                        Spacer()
-                    )
-                        .setMargin("1rem 0 0"),
-                    Horizontal(
-                        PlainText("Forgot your Password?"),
-                        Button("Reset it here")
-                            .setStyle(ButtonStyle.Inline)
-                            .setColor(Color.Colored)
-                            .onPromiseClick(async () => {
-                                await API.auth.forgotPassword.post({
-                                    email: formData.get("email")?.toString() ?? ""
-                                });
-                            })
-                            .addClass("link"),
-                        Spacer()
-                    )
+                    ...(<{ [ key in NonNullable<typeof state.mode> ]: Component[]; }>{
+                        login: [
+                            Input({ placeholder: "Email", type: "email", ...syncFromData(formData, "email") }),
+                            Input({ placeholder: "Password", type: "password", ...syncFromData(formData, "password"), value: state.password }),
+                            Button("Login")
+                                .onPromiseClick(handleSignUpInButton(formData, state, update))
+                                .setJustify("center"),
+                            Horizontal(
+                                PlainText("New here?"),
+                                Button("Create a Account")
+                                    .setStyle(ButtonStyle.Inline)
+                                    .onClick(() => update({ mode: "register" }))
+                                    .setColor(Color.Colored)
+                                    .addClass("link"),
+                                Spacer()
+                            )
+                                .setMargin("1rem 0 0"),
+                            Horizontal(
+                                PlainText("Forgot your Password?"),
+                                Button("Reset it here")
+                                    .setStyle(ButtonStyle.Inline)
+                                    .setColor(Color.Colored)
+                                    .onClick(() => update({ mode: state.mode == "login" ? "reset-password" : "login" }))
+                                    .addClass("link"),
+                                Spacer()
+                            )
+                        ],
+                        register: [
+                            Input({ placeholder: "Name", type: "text", ...syncFromData(formData, "name") }),
+                            Input({ placeholder: "Email", type: "email", ...syncFromData(formData, "email") }),
+                            Input({ placeholder: "Password", type: "password", ...syncFromData(formData, "password"), value: state.password }),
+                            Button("Register")
+                                .onPromiseClick(handleSignUpInButton(formData, state, update))
+                                .setJustify("center"),
+                            Horizontal(
+                                PlainText("Known here?"),
+                                Button("Sign in")
+                                    .setStyle(ButtonStyle.Inline)
+                                    .onClick(() => update({ mode: "login" }))
+                                    .setColor(Color.Colored)
+                                    .addClass("link"),
+                                Spacer()
+                            )
+                                .setMargin("1rem 0 0"),
+                        ],
+                        "reset-password": [
+                            Input({ placeholder: "Email", type: "email", ...syncFromData(formData, "email") }),
+                            Button("Reset")
+                                .onPromiseClick(async () => {
+                                    await API.auth.forgotPassword.post({
+                                        email: formData.get("email")?.toString() ?? ""
+                                    });
+                                })
+                                .setJustify("center"),
+                            Horizontal(
+                                PlainText("Known here?"),
+                                Button("Sign in")
+                                    .setStyle(ButtonStyle.Inline)
+                                    .onClick(() => update({ mode: "login" }))
+                                    .setColor(Color.Colored)
+                                    .addClass("link"),
+                                Spacer()
+                            )
+                                .setMargin("1rem 0 0"),
+                        ]
+                    })[ state.mode ?? "login" ],
                 ]).disableAutoSpacerAtBottom().getComponents();
             })()
         ).setGap("11px"),
@@ -81,6 +118,7 @@ View<{ error?: string, signup?: boolean, resetToken?: string, loading: boolean, 
     Custom(img(heroImage)).addClass("background-image")
 ))
     .change(({ update }) => {
+        update({ mode: "login" });
         if (type == "google" && state && code) {
             update({ loading: true });
             API.auth.google.post({ code, state }).then(async x => {
