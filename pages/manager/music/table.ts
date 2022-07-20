2b445100ffaf49c66b00b8a50ba577d01fd20b31
lucsoft <mail@lucsoft.de>
2022-07-20 21:34:51 +0200

    feature(wizard): Add inline text input

--- pages/manager/music/table.ts
+++ pages/manager/music/table.ts
@@ -1,39 +1,78 @@
-import { Box, ButtonStyle, Checkbox, Color, Component, createElement, Custom, Dialog, DropDownInput, IconButton, img, Input, PlainText, View } from "../../../deps.ts";
-import { EditArtists, getYearList, stringToColour, syncFromData } from "../helper.ts";
+import { Box, ButtonStyle, Checkbox, Color, Component, createElement, Custom, DropDownInput, IconButton, img, InputForm, PlainText, View } from "../../../deps.ts";
+import { EditArtists, getYearList, stringToColour } from "../helper.ts";
 import { ColumEntry } from "../types.ts";
 
 import primary from "../../../data/primary.json" assert { type: "json"};
 import language from "../../../data/language.json" assert { type: "json"};
+import { accessibilityDisableTabOnDisabled } from "https://raw.githubusercontent.com/lucsoft/WebGen/1144da3a8dbcfb22253fb5c4bc8b3f92c4f208bf/src/lib/Accessibility.ts";
 function ProfilePicture(component: Component, name: string) {
-    const ele = component.draw()
+    const ele = component.draw();
     ele.style.backgroundColor = stringToColour(name);
-    return Custom(ele).addClass("profile-picture")
+    return Custom(ele).addClass("profile-picture");
 }
 
-export const TableDef = (formData: FormData) => <ColumEntry<{ Id: string }>[]>[
+// TODO: Move to WebGen and change content editable as inputs have a stupid size width limit and not just auto with
+export class InlineTextInput extends InputForm<string> {
+    constructor(color: Color = Color.Grayscaled) {
+        super();
+        this.wrapper.classList.add("winput", "inline", "has-value", color ?? Color.Grayscaled);
+        this.wrapper.tabIndex = accessibilityDisableTabOnDisabled();
+        const input = createElement("input") as HTMLInputElement;
+
+        this.wrapper.append(input);
+        input.addEventListener("change", () => {
+            this.setValue(input.value);
+        });
+
+        this.addEventListener("update", (event) => {
+            const value = (<CustomEvent<string>>event).detail;
+            input.value = value;
+            if (this.formData && this.key)
+                this.formData.set(this.key, this.saveData(value));
+
+        });
+        this.wrapper.classList.add("isList");
+    }
+    saveData(data: string): FormDataEntryValue {
+        return data;
+    }
+    parseData(data: FormDataEntryValue): string {
+        return data.toString();
+    }
+    setStyle(_style: ButtonStyle): this {
+        throw new Error("Method not implemented.");
+    }
+    setColor(_color: Color): this {
+        throw new Error("Method not implemented.");
+    }
+
+}
+export const TableDef = (formData: FormData) => <ColumEntry<{ Id: string; }>[]>[
     [ "Title", "auto", ({ Id }) =>
         formData.has(`song-${Id}-progress`) ? Box(
             Custom((() => {
-                const element = createElement("progress")
+                const element = createElement("progress");
                 element.max = 100;
-                element.value = parseFloat(formData.get(`song-${Id}-progress`)?.toString() ?? "")
+                element.value = parseFloat(formData.get(`song-${Id}-progress`)?.toString() ?? "");
                 return element;
             })())
         ) :
             // TODO: Refactor this to InlineTextInput() and add a custom size
             View(({ update }) =>
-                PlainText(formData.get(`song-${Id}-title`)?.toString() ?? "-").setFont(1, 500)
-                    .onClick(() => {
-                        Dialog(() => Input({
-                            placeholder: "title",
-                            ...syncFromData(formData, `song-${Id}-title`),
-                        }))
-                            .onClose(() => update({}))
-                            .setTitle("Change Title")
-                            .addButton("Update", "close")
-                            .allowUserClose()
-                            .open()
-                    })
+                new InlineTextInput()
+                    .syncFormData(formData, `song-${Id}-title`),
+                // PlainText(formData.get(`song-${Id}-title`)?.toString() ?? "-").setFont(1, 500)
+                //     .onClick(() => {
+                //         Dialog(() => Input({
+                //             placeholder: "title",
+                //             ...syncFromData(formData, `song-${Id}-title`),
+                //         }))
+                //             .onClose(() => update({}))
+                //             .setTitle("Change Title")
+                //             .addButton("Update", "close")
+                //             .allowUserClose()
+                //             .open();
+                //     })
             ).asComponent()
     ],
     [ "Artists", "max-content", ({ Id }) =>
@@ -47,8 +86,8 @@ export const TableDef = (formData: FormData) => <ColumEntry<{ Id: string }>[]>[
             .onClick(() => {
                 EditArtists(formData.get(`song-${Id}-artists`) ? JSON.parse(formData.get(`song-${Id}-artists`)!.toString()) : [ [ "", "", "PRIMARY" ] ]).then((x) => {
                     formData.set(`song-${Id}-artists`, JSON.stringify(x));
-                    update({})
-                })
+                    update({});
+                });
             })
         ).asComponent()
     ],
