f95528e688cb8478631c3b4cb528d501bc8d7d32
lucsoft <mail@lucsoft.de>
2022-08-07 11:14:42 +0200

    more mobile tweaking

--- pages/manager/misc/Entry.ts
+++ pages/manager/misc/Entry.ts
@@ -1,26 +1,46 @@
-import { CenterV, Horizontal, Icon, PlainText, Spacer } from "webgen/mod.ts";
+import { CenterV, Grid, Horizontal, Icon, PlainText, Spacer } from "webgen/mod.ts";
 import { MediaQuery } from "../helper.ts";
 
 export function Entry(text: string, subtext?: string, action?: () => void) {
-    return MediaQuery("(max-width: 500px)", (small) =>
-        Horizontal(
-            CenterV(
-                PlainText(text)
-                    .setFont(small ? 1.3 : 1.5, 700),
-                ...subtext ? [
-                    Spacer(),
-                    PlainText(subtext)
-                        .setFont(small ? .8 : 1, 700)
-                        .addClass("subtitle")
-                ] : []
+    return MediaQuery("(max-width: 520px)", (small) =>
+        small
+            ? Grid(
+                CenterV(
+                    PlainText(text)
+                        .setFont(small ? 1.3 : 1.5, 700),
+                    ...subtext ? [
+                        Spacer(),
+                        PlainText(subtext)
+                            .setFont(small ? .8 : 1, 700)
+                            .addClass("subtitle")
+                    ] : []
+                )
+                    .addClass("meta-data"),
+                action ? CenterV(Icon("arrow_forward_ios")) : Spacer()
             )
-                .addClass("meta-data"),
+                .setRawColumns("auto max-content")
+                .onClick((() => { action?.(); }))
+                .setPadding("10px 18px")
+                .addClass("list-entry", action ? "action" : "no-actions", "limited-width")
 
-            Spacer(),
-            action ? CenterV(Icon("arrow_forward_ios")) : null
-        )
-            .onClick((() => { action?.(); }))
-            .setPadding(small ? "10px 18px" : "18px 24px")
-            .addClass("list-entry", action ? "action" : "no-actions", "limited-width")
+            : Horizontal(
+                CenterV(
+                    PlainText(text)
+                        .setFont(1.5, 700),
+                    ...subtext ? [
+                        Spacer(),
+                        PlainText(subtext)
+                            .setFont(1, 700)
+                            .addClass("subtitle")
+                    ] : []
+                )
+                    .addClass("meta-data"),
+
+                Spacer(),
+                action ? CenterV(Icon("arrow_forward_ios")) : null
+            )
+                .onClick((() => { action?.(); }))
+                .setPadding("18px 24px")
+                .addClass("list-entry", action ? "action" : "no-actions", "limited-width")
     );
 }
