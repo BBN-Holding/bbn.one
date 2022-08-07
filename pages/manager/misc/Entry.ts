1ba5b250fe424a2e520e29e304bc982891a6f328
lucsoft <mail@lucsoft.de>
2022-08-07 10:48:30 +0200

    fix(Entry): Made it mobile responsive

--- pages/manager/misc/Entry.ts
+++ pages/manager/misc/Entry.ts
@@ -1,22 +1,26 @@
 import { CenterV, Horizontal, Icon, PlainText, Spacer } from "webgen/mod.ts";
+import { MediaQuery } from "../helper.ts";
 
 export function Entry(text: string, subtext?: string, action?: () => void) {
-    return Horizontal(
-        CenterV(
-            PlainText(text)
-                .setFont(1.5, 700),
-            ...subtext ? [
-                Spacer(),
-                PlainText(subtext)
-                    .setFont(1, 700)
-                    .addClass("subtitle")
-            ] : []
+    return MediaQuery("(max-width: 500px)", (small) =>
+        Horizontal(
+            CenterV(
+                PlainText(text)
+                    .setFont(small ? 1.3 : 1.5, 700),
+                ...subtext ? [
+                    Spacer(),
+                    PlainText(subtext)
+                        .setFont(small ? .8 : 1, 700)
+                        .addClass("subtitle")
+                ] : []
+            )
+                .addClass("meta-data"),
+
+            Spacer(),
+            action ? CenterV(Icon("arrow_forward_ios")) : null
         )
-            .addClass("meta-data"),
-        Spacer(),
-        action ? CenterV(Icon("arrow_forward_ios")) : null
-    )
-        .onClick((() => { action?.(); }))
-        .setPadding("18px 24px")
-        .addClass("list-entry", action ? "action" : "no-actions", "limited-width");
+            .onClick((() => { action?.(); }))
+            .setPadding(small ? "10px 18px" : "18px 24px")
+            .addClass("list-entry", action ? "action" : "no-actions", "limited-width")
+    );
 }
