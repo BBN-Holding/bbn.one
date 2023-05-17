86e54104360c42785cadd06f2827f6ac28c5b4af
GregTCLTK <Skidder@bbn.one>
2023-05-18 00:25:44 +0200

    adjust powerstates

--- spec/music.ts
+++ spec/music.ts
@@ -201,7 +201,7 @@ export enum ServerTypes {
     Bedrock = "/minecraft/bedrock/"
 }
 
-export const powerState = zod.enum([ "start", "stop", "restart", "kill", "installing" ]);
+export const powerState = zod.enum([ "offline", "starting", "stopping", "running", "installing" ]);
 
 export const server = zod.object({
     _id: zod.string(),
