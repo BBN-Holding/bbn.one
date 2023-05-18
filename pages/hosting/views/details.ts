588d408cf0b93bca11a219b8e278680cf7d1fbc8
lucsoft <mail@lucsoft.de>
2023-05-18 15:40:35 +0200

    fix: added richer details

--- pages/hosting/views/details.ts
+++ pages/hosting/views/details.ts
@@ -1,76 +1,55 @@
-import { Card, Grid, PlainText, Reactive } from "webgen/mod.ts";
+import { Card, Grid, MediaQuery, PlainText, Reactive } from "webgen/mod.ts";
 import { MB, state } from "../data.ts";
 import './details.css';
 import { format } from "std/fmt/bytes.ts";
 
-export const detailsView = () => Reactive(state, "meta", () => Grid(
-    Card(
-        Grid(
-            PlainText(state.meta.coins.toLocaleString())
-                .setFont(2, 700),
-            PlainText("Coins")
-                .setFont(1, 700)
-                .addClass("gray-color")
+export const detailsView = () =>
+    MediaQuery("(max-width: 700px)", (small) =>
+        Reactive(state, "meta", () =>
+            Grid(
+                Card(
+                    Grid(
+                        PlainText(state.meta.coins.toLocaleString())
+                            .setFont(2, 700),
+                        PlainText("Coins")
+                            .setFont(1, 700)
+                            .addClass("gray-color")
+                    )
+                        .addClass("details-item")
+                ),
+                Card(
+                    Grid(
+                        PlainText(state.meta.used.slots + " / " + state.meta.limits.slots)
+                            .setFont(2, 700),
+                        PlainText("Servers")
+                            .setFont(1, 700)
+                            .addClass("gray-color")
+                    )
+                        .addClass("details-item")
+                ),
+                Card(
+                    Grid(
+                        PlainText(format(state.meta.used.memory * MB) + " / " + format(state.meta.limits.memory * MB))
+                            .setFont(2, 700),
+                        PlainText("Memory")
+                            .setFont(1, 700)
+                            .addClass("gray-color")
+                    )
+                        .addClass("details-item")
+                ),
+                Card(
+                    Grid(
+                        PlainText(format(state.meta.used.disk * MB) + " / " + format(state.meta.limits.disk * MB))
+                            .setFont(2, 700),
+                        PlainText("Disk")
+                            .setFont(1, 700)
+                            .addClass("gray-color")
+                    )
+                        .addClass("details-item")
+                ),
+            )
+                .setEvenColumns(small ? 1 : 2)
+                .setGap("var(--gap)")
+                .addClass("limited-width", "details-grid")
         )
-            .addClass("details-item")
-    ),
-
-    Card(
-        Grid(
-            PlainText(format(state.meta.limits.memory * MB) + " / " + format(state.meta.limits.memory * MB))
-                .setFont(2, 700),
-            PlainText("Memory")
-                .setFont(1, 700)
-                .addClass("gray-color")
-        )
-            .addClass("details-item")
-    ),
-
-    Card(
-        Grid(
-            PlainText(format(state.meta.limits.disk * MB) + " / " + format(state.meta.limits.disk * MB))
-                .setFont(2, 700),
-            PlainText("Disk")
-                .setFont(1, 700)
-                .addClass("gray-color")
-        )
-            .addClass("details-item")
-    ),
-    // AYO THIS SHIT MISSING!?!?
-    // Card(
-    //     Grid(
-    //         PlainText(state.meta.dbs[ 0 ] + " / " + state.meta.dbs[ 1 ])
-    //             .setFont(2, 700),
-    //         PlainText("Databases")
-    //             .setFont(1, 700)
-    //             .addClass("gray-color")
-    //     )
-    //         .addClass("details-item")
-    // ),
-
-    // Card(
-    //     Grid(
-    //         PlainText(state.meta.slots[ 0 ] + " / " + state.meta.slots[ 1 ])
-    //             .setFont(2, 700),
-    //         PlainText("Servers")
-    //             .setFont(1, 700)
-    //             .addClass("gray-color")
-    //     )
-    //         .addClass("details-item")
-    // ),
-
-    // Card(
-    //     Grid(
-    //         PlainText(state.meta.ports[ 0 ] + " / " + state.meta.ports[ 1 ])
-    //             .setFont(2, 700),
-    //         PlainText("Endpoints (Ports)")
-    //             .setFont(1, 700)
-    //             .addClass("gray-color")
-    //     )
-    //         .addClass("details-item")
-    // )
-)
-    .setDynamicColumns(6, "20rem")
-    .setGap("var(--gap)")
-    .addClass("limited-width", "details-grid")
-);
+    );
