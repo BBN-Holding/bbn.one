96e69b2f01821cb0b493cd084917feb059e9c52c
Schlauer-Hax <hax@bbn.family>
2023-05-18 22:58:44 +0200

    test

--- pages/manager/misc/redirect.ts
+++ pages/manager/misc/redirect.ts
@@ -3,6 +3,7 @@ if (location.pathname.includes("google")) {
     params.set("type", "google");
 } else if (location.pathname.includes("discord")) {
     params.set("type", "discord");
+    localStorage.setItem("goal", atob(params.get("state") ?? "")?.replace('https://bbn.one', '') || "/music");
 }
 
 location.href = "/signin?" + params.toString();
