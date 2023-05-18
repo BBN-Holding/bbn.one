e8a155e2b1c1c88ee4d50b5ecb0ccbd7430af8d4
GregTCLTK <Skidder@bbn.one>
2023-05-18 15:41:02 +0200

    fix: make zendesk work again

--- pages/manager/misc/zendesk.ts
+++ pages/manager/misc/zendesk.ts
@@ -1,8 +1,4 @@
 import { API } from "../RESTSpec.ts";
-import { Redirect, RegisterAuthRefresh } from "../helper.ts";
-
-Redirect();
-await RegisterAuthRefresh();
 
 const para = new URLSearchParams(location.search);
 const { brand_id, locale_id, return_to, timestamp } = {
