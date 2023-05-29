76ab512a5735fda509efa2ea7a8973c84bdc788f
GregTCLTK <Skidder@bbn.one>
2023-05-29 11:51:31 +0200

    instrumental is no longer optional

--- spec/music.ts
+++ spec/music.ts
@@ -68,7 +68,7 @@ export const song = zod.object({
     country: zod.string().optional(),
     language: zod.string().optional(),
     explicit: zod.boolean(),
-    instrumental: zod.boolean().optional(),
+    instrumental: zod.boolean(),
     file: zod.string({ required_error: "a Song is missing its file." }),
     progress: zod.number().optional().transform(x => <typeof x>undefined)
 })
