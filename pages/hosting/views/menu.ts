d61ef1111fd93206628fd680d3b25d4cc0e4b9d4
Schlauer-Hax <hax@bbn.family>
2023-10-01 01:13:41 +0200

    feat: added more filetypes

--- pages/hosting/views/menu.ts
+++ pages/hosting/views/menu.ts
@@ -767,58 +767,77 @@ const fileNameIncludes: Record<string, string> = {
     "config": "gear"
 }
 
-// filetype-aac
-// filetype-ai
-// filetype-bmp
-// filetype-cs
-// filetype-css
-// filetype-csv
-// filetype-doc
-// filetype-docx
-// filetype-exe
-// filetype-gif
-// filetype-heic
-// filetype-html
-// filetype-java
-// filetype-jpg
-// filetype-js
-// filetype-json
-// filetype-jsx
-// filetype-key
-// filetype-m4p
-// filetype-md
-// filetype-mdx
-// filetype-mov
-// filetype-mp3
-// filetype-mp4
-// filetype-otf
-// filetype-pdf
-// filetype-php
-// filetype-png
-// filetype-ppt
-// filetype-pptx
-// filetype-psd
-// filetype-py
-// filetype-raw
-// filetype-rb
-// filetype-sass
-// filetype-scss
-// filetype-sh
-// filetype-sql
-// filetype-svg
-// filetype-tiff
-// filetype-tsx
-// filetype-ttf
-// filetype-txt
-// filetype-wav
-// filetype-woff
-// filetype-xls
-// filetype-xlsx
-// filetype-xml
-// filetype-yml
+const supportedFiletypes = [
+    'aac',
+    'ai',
+    'bmp',
+    'cs',
+    'css',
+    'csv',
+    'doc',
+    'docx',
+    'exe',
+    'gif',
+    'heic',
+    'html',
+    'java',
+    'jpg',
+    'js',
+    'json',
+    'jsx',
+    'key',
+    'm4p',
+    'md',
+    'mdx',
+    'mov',
+    'mp3',
+    'mp4',
+    'otf',
+    'pdf',
+    'php',
+    'png',
+    'ppt',
+    'pptx',
+    'psd',
+    'py',
+    'raw',
+    'rb',
+    'sass',
+    'scss',
+    'sh',
+    'sql',
+    'svg',
+    'tiff',
+    'tsx',
+    'ttf',
+    'txt',
+    'wav',
+    'woff',
+    'xls',
+    'xlsx',
+    'xml',
+    'yml',
+]
+
+const otherFiletypes: Record<string, string> = {
+    'zip': 'archive',
+    'tar': 'archive',
+    'gz': 'archive',
+    '7z': 'archive',
+    'rar': 'archive',
+    'yaml': 'filetype-yml',
+    'jar': 'filetype-java',
+}
 function mapFiletoIcon(file: RemotePath) {
     if (!file.fileMimeType) {
         return fileNameIncludes[file.name] ?? "folder";
     }
-    return "filetype-" + file.fileMimeType.split(';')[0].split('/')[1].split('-').at(-1);
+    //const filetype = file.fileMimeType.split(';')[0].split('/')[1].split('-').at(-1);
+    const filetype = file.name.split('.').at(-1);
+    if (!filetype) return "file-earmark";
+    if (supportedFiletypes.includes(filetype))
+        return "filetype-" + filetype;
+    if (otherFiletypes[filetype])
+        return otherFiletypes[filetype];
+    return "file-earmark";
 }
