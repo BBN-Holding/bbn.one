531b5c7558b71e8a1a19d69ca8185e46f5a1006b
Schlauer-Hax <hax@bbn.family>
2024-03-16 19:29:25 +0100

    feat: charts

--- pages/shared/components.ts
+++ pages/shared/components.ts
@@ -1,3 +1,16 @@
-import { Box, Custom, loadingWheel } from "webgen/mod.ts";
+import type { ChartConfiguration, ChartConfigurationCustomTypesPerDataset, ChartType, DefaultDataPoint, } from "https://esm.sh/chart.js/auto";
+import { Box, Custom, lazyInit, loadingWheel } from "webgen/mod.ts";
 
 export const LoadingSpinner = () => Box(Custom(loadingWheel() as Element as HTMLElement)).addClass("loading");
+
+const lazyChart = lazyInit(() => import("https://esm.sh/chart.js/auto"));
+
+export const Chart = <TType extends ChartType = ChartType, TData = DefaultDataPoint<TType>, TLabel = unknown>
+    (config: ChartConfiguration<TType, TData, TLabel> | ChartConfigurationCustomTypesPerDataset<TType, TData, TLabel>) => {
+    const canvas = document.createElement("canvas");
+
+    console.log(config);
+    lazyChart().then(chartjs => new chartjs.Chart(canvas, config));
+
+    return Box(Custom(canvas).addClass("chart"));
+};
